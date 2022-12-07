a6d7dc87a0a1c2596a8ad074d84177cbbd2f9ee8
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-12-08 01:07:39 +0900

    first commit

--- Formula/xccontributerank.rb
+++ Formula/xccontributerank.rb
@@ -1,4 +1,4 @@
- Documentation: https://docs.brew.sh/Formula-Cookbook
+# Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Xccontributerank < Formula
